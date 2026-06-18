b3e3304b06e1241a85f6cae4c2fc272b2f860cda
Shariqua Hazra <shariquahazra@gmail.com>
2026-06-18 13:48:53 +0530

    fix pgx database driver for neon compatibility

--- internal/db/db.go
+++ internal/db/db.go
@@ -4,12 +4,12 @@ import (
 	"database/sql"
 	"log"
 
-	_ "github.com/lib/pq"
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 func Connect(connStr string) *sql.DB {
 
-	db, err := sql.Open("postgres", connStr)
+	db, err := sql.Open("pgx", connStr)
 	if err != nil {
 		log.Fatal(err)
 	}
