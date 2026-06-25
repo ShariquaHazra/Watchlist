495c66bda63c7e8a76437026a79560faefbc41b9
Vishal <vishalsingh4328m@gmail.com>
2026-06-25 13:19:55 +0530

    updated with golang migrate

--- go.mod
+++ go.mod
@@ -5,7 +5,9 @@ go 1.26.4
 require (
 	github.com/go-playground/validator/v10 v10.30.3
 	github.com/golang-jwt/jwt/v5 v5.3.1
+	github.com/golang-migrate/migrate/v4 v4.19.1
 	github.com/gorilla/mux v1.8.1
+	github.com/gorilla/websocket v1.5.3
 	github.com/jackc/pgx/v5 v5.10.0
 	github.com/joho/godotenv v1.5.1
 	golang.org/x/crypto v0.52.0
@@ -15,11 +17,11 @@ require (
 	github.com/gabriel-vasile/mimetype v1.4.13 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
 	golang.org/x/sync v0.20.0 // indirect
 	golang.org/x/sys v0.45.0 // indirect
 	golang.org/x/text v0.37.0 // indirect
--- internal/db/migrate.go
+++ internal/db/migrate.go
@@ -2,77 +2,35 @@ package db
 
 import (
 	"database/sql"
-	"fmt"
-	"os"
-	"path/filepath"
-	"strings"
+	"errors"
+	"log"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
 func RunMigrations(database *sql.DB) error {
-
-	_, err := database.Exec(`
-		CREATE TABLE IF NOT EXISTS migrations (
-			id SERIAL PRIMARY KEY,
-			name TEXT UNIQUE NOT NULL,
-			applied_at TIMESTAMP DEFAULT NOW()
-		)
-	`)
+	driver, err := postgres.WithInstance(database, &postgres.Config{})
 	if err != nil {
 		return err
 	}
 
-	migrationPath := "internal/db/migrations"
-
-	files, err := os.ReadDir(migrationPath)
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://internal/db/migrations", // migrations folder
+		"postgres",
+		driver,
+	)
 	if err != nil {
 		return err
 	}
 
-	for _, file := range files {
-
-		if !strings.HasSuffix(file.Name(), ".up.sql") {
-			continue
+	if err := m.Up(); err != nil {
+		if errors.Is(err, migrate.ErrNoChange) {
+			log.Println("No new migrations to run")
+			return nil
 		}
-
-		var exists bool
-
-		err = database.QueryRow(
-			"SELECT EXISTS (SELECT 1 FROM migrations WHERE name=$1)",
-			file.Name(),
-		).Scan(&exists)
-
-		if err != nil {
-			return err
-		}
-
-		if exists {
-			fmt.Println("Skipping:", file.Name())
-			continue
-		}
-
-		filePath := filepath.Join(migrationPath, file.Name())
-
-		sqlBytes, err := os.ReadFile(filePath)
-		if err != nil {
-			return err
-		}
-
-		fmt.Println("Running migration:", file.Name())
-
-		_, err = database.Exec(string(sqlBytes))
-		if err != nil {
-			return err
-		}
-
-		_, err = database.Exec(
-			"INSERT INTO migrations(name) VALUES($1)",
-			file.Name(),
-		)
-		if err != nil {
-			return err
-		}
-
-		fmt.Println("Applied:", file.Name())
+		return err // Duplicate version → error aayega
 	}
 
 	return nil
